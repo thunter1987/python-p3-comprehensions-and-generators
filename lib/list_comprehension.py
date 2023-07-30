2614f606afa5e25bdfd6c02d6484c24b80491414
Tony <thunter1987@yahoo.com>
2023-07-29 22:05:05 -0500

    passes tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list_evens = [num for num in num_list if num%2 ==0]
+    return num_list_evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [sentence + "!" for sentence in sentence_list]
+    return new_sentence_list
